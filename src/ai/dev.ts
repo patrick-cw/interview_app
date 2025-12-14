b9cde0afae14d715ce8f3ade39648e48b7685b4b
patrick-cw <57954969+patrick-cw@users.noreply.github.com>
2025-12-14 17:39:44 +0700

    remove unused

--- src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-iq-test';
-import '@/ai/flows/generate-psycho-test';
-import '@/ai/flows/receive-targeted-feedback';
-import '@/ai/flows/simulate-interview';
